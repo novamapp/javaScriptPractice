d10077a52fb9972afebd9e2fad0e18e87d7a76fd
novamapp <ogheneovoabu@gmail.com>
2022-05-21 23:31:53 -0500

    how to use spread notes

--- notes.js
+++ notes.js
@@ -1,10 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("ready");
     
-    printTimes(3);
-    console.log(addNums(3, 6));
-    console.log(addToN(3));
-    console.log(addToN(9, 3));
+    applySpread();
 });
 
 /*
@@ -26,20 +23,10 @@ let: like var, excepted can be scoped to: block, statement, or expression
 
 */
 
-// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/es6/use-arrow-functions-to-write-concise-anonymous-functions
-// can use arrow functions in place of anonymous functions
-const afunc = () => {
-    console.log("This is an arrow function");
+// can use spread to copy array
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/es6/use-the-spread-operator-to-evaluate-arrays-in-place
+function applySpread() {
+    let arr1 = ["asd", "sfsdf", "sdfsdf", "werwe"];
+    let copyArr1 = [...arr1];
+    console.log("Copy: ", copyArr1);
 }
-
-// single parameter for arrow function
-const printTimes = times => {
-    for(let i=0; i<times; i++) afunc();
-}
-
-// more than one parameter for arrow function
-const addNums = (num1, num2) => num1+" + "+num2+" = "+(num1 + num2);
-
-// arrow function with default parameter
-// the default parameters must be later in the list of parameters
-const addToN = (n, inc=0) => n+" + "+inc+" = "+(n+inc);
