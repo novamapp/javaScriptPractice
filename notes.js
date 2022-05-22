af125331fb2ae8916bcb4851c93355eb9f0c26dd
novamapp <ogheneovoabu@gmail.com>
2022-05-22 01:38:23 -0500

    destructure objs, arrays, function parameter notes

--- notes.js
+++ notes.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("ready");
     
-    applySpread();
+    destructuringObjectExample();
+    destructingArrayExample();
+    destructuredFunctionParameter();
 });
 
 /*
@@ -23,10 +25,45 @@ let: like var, excepted can be scoped to: block, statement, or expression
 
 */
 
-// can use spread to copy array
-// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/es6/use-the-spread-operator-to-evaluate-arrays-in-place
-function applySpread() {
-    let arr1 = ["asd", "sfsdf", "sdfsdf", "werwe"];
-    let copyArr1 = [...arr1];
-    console.log("Copy: ", copyArr1);
+// destructuring assignment to assign variables from objects
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/es6/use-destructuring-assignment-to-assign-variables-from-nested-objects
+function destructuringObjectExample() {
+    const anObj = {
+        firstPlace: {
+            firstName: "Sidney",
+            lastName: "Star",
+            score: 123
+        },
+        secondPlace: {
+            firstName: "Sidney",
+            lastName: "Star",
+            score: 123
+        }
+    };
+
+    const { firstPlace: { firstName: name, score }} = anObj;
+    console.log(name+" won first place with score: ", score);
 }
+
+// destructuring assignment from an array
+// difference between destructuring array and spreading an array is:
+//      - spreading unpacks the whole
+//      - destructuring allows selection of only elements that are to be assigned to a variable
+function destructingArrayExample() {
+    const arr = [1, 2, 3, 4, 5, 6];
+    const [first,, third,, fifth] = arr;
+    console.log("first: "+first+", \tthird: "+third+" \tfifth: "+fifth);
+}
+
+// passing destructured object directly into a function
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/es6/use-destructuring-assignment-to-pass-an-object-as-a-functions-parameters
+function destructuredFunctionParameter() {
+    const statement = ({name, category}) => "A "+name+" is a "+category;
+    console.log(statement(foodItem));
+}
+
+const foodItem = {
+    name: "banana",
+    color: "yellow",
+    category: "fruit"
+};
